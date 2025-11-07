ff8a49178a63901d55d54578c888f02ca1ea3e93
jubeormk1 <jubeormk1@gmail.com>
2025-11-07 12:31:14 +1100

    [skip ci] WIP: BUG in sftphandler process keeps responding to the same read dir request

--- demo/sftp/std/src/main.rs
+++ demo/sftp/std/src/main.rs
@@ -206,20 +206,19 @@ async fn listen(
 async fn main(spawner: Spawner) {
     env_logger::builder()
         .filter_level(log::LevelFilter::Info)
-        .filter_module("sunset::runner", log::LevelFilter::Info)
-        // .filter_module("sunset::runner", log::LevelFilter::Trace)
-        // .filter_module("sunset::channel", log::LevelFilter::Trace)
-        .filter_module(
-            "sunset_sftp::sftphandler::sftpoutputchannelhandler",
-            log::LevelFilter::Debug,
-        )
-        .filter_module("sunset_sftp::sftpsink", log::LevelFilter::Info)
-        .filter_module("sunset_sftp::sftpsource", log::LevelFilter::Info)
         .filter_module(
             "sunset_demo_sftp_std::demosftpserver",
             log::LevelFilter::Debug,
         )
+        .filter_module("sunset_sftp::sftphandler", log::LevelFilter::Debug)
+        .filter_module(
+            "sunset_sftp::sftphandler::sftpoutputchannelhandler",
+            log::LevelFilter::Info,
+        )
+        .filter_module("sunset_sftp::sftpsink", log::LevelFilter::Info)
+        .filter_module("sunset_sftp::sftpsource", log::LevelFilter::Info)
         .filter_module("sunset_sftp::sftpserver", log::LevelFilter::Debug)
+        .filter_module("sunset::runner", log::LevelFilter::Info)
         .filter_module("sunset::encrypt", log::LevelFilter::Info)
         .filter_module("sunset::conn", log::LevelFilter::Info)
         .filter_module("sunset::kex", log::LevelFilter::Trace)
